0d0ac32a57875b8d57dd7bffc32aad4bb0b3b985
XeuTap <ctehkas@gmail.com>
2024-02-16 08:43:48 +0200

    Changed default encoding to UTF-8 according to QT documentation

--- pyqt6rc/script_helpers.py
+++ pyqt6rc/script_helpers.py
@@ -4,5 +4,4 @@ import logging
 def set_logger(disabled: bool = False) -> None:
     logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
     logger = logging.getLogger("main")
-    logger.info("sadads")
     logger.disabled = disabled
